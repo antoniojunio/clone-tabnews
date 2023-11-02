0cf7afc17679d24f4d0740f48c021aa87e4bae5e
antoniojunio <juniordsj@gmail.com>
2023-11-01 22:20:51 -0300

    modifica a frase

--- pages/index.js
+++ pages/index.js
@@ -1,5 +1,5 @@
 function Home () {
-  return <h1>Geovana, eu amo você. Se você me ama, dá uma risadinha!</h1>
+  return <h1>Geovana, eu amo você. Se você me ama, dá uma risadinha! Se deu uma risadinha, faz uma salpituquinha!</h1>
 }
 
 export default Home
