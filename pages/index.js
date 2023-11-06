cefbfcb1333b236a9bbbcf5f07c342db674f727f
antoniojunio <juniordsj@gmail.com>
2023-11-05 21:32:05 -0300

    adiciona scripts `lint:check` e `lint:fix`

--- pages/index.js
+++ pages/index.js
@@ -1,5 +1,10 @@
-function Home () {
-  return <h1>Geovana, eu amo você. Se você me ama, dá uma risadinha! Se deu uma risadinha, faz uma salpituquinha!</h1>
+function Home() {
+  return (
+    <h1>
+      Geovana, eu amo você. Se você me ama, dá uma risadinha! Se deu uma
+      risadinha, faz uma salpituquinha!
+    </h1>
+  );
 }
 
-export default Home
+export default Home;
